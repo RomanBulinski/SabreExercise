c7df11090293530e018868062d93eda74079538f
Roman Bulinski <aboandrus@gmail.com>
2019-08-01 10:07:06 +0200

    Implemented code

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,37 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class App {
+
+    public String indexing(String input) {
+        
+
+        String cleanedInput = input.replaceAll("[^a-zA-Z0-9 ]", "").toLowerCase();
+        String[] words = cleanedInput.split(" ");
+        TreeMap<String, List<String>> indexes = new TreeMap<>();
+        Set<String> lettersSet = new HashSet<>(Arrays.asList(cleanedInput.replaceAll(" ", "").split("")));
+        lettersSet.forEach(n -> indexes.put(n, new ArrayList<String>()));
+
+        for (String word : words) {
+            String[] letters = word.split("");
+            for (String s : letters) {
+                List<String> tempWords = indexes.get(s);
+                if (!tempWords.contains(word)) {
+                    tempWords.add(word);
+                }
+                Collections.sort(tempWords);
+                indexes.put(s, tempWords);
+            }
+        }
+
+        String result = indexes.keySet().stream()
+                .map(m -> m + ": " + indexes.get(m).toString()
+                        .replaceAll("\\[", "")
+                        .replaceAll("]", "") + "\n")
+                .collect(Collectors.joining());
+
+        return result.substring(0, result.length() - 1);
+    }
+
+
 }
