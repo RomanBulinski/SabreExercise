bd05095771bcb45f131dc14a92fcffa7540b77b8
Roman Bulinski <aboandrus@gmail.com>
2019-08-01 17:37:56 +0200

    Added new test - from demo

--- src/test/java/TestApp.java
+++ src/test/java/TestApp.java
@@ -37,8 +37,8 @@ public class TestApp {
 
     @Test
     public void isThreeIndexFromThreeLetterWord() {
-        String result = "a: abc\n"+
-                "b: abc\n"+
+        String result = "a: abc\n" +
+                "b: abc\n" +
                 "c: abc";
         App app = new App();
         assertEquals(result, app.indexing("abc"));
@@ -46,7 +46,7 @@ public class TestApp {
 
     @Test
     public void isTwoIndexFromTwoLetterOneWord() {
-        String result = "a: ab\n"+
+        String result = "a: ab\n" +
                 "b: ab";
         App app = new App();
         assertEquals(result, app.indexing("ab"));
@@ -54,9 +54,33 @@ public class TestApp {
 
     @Test
     public void isTwoIndexFromTwoLetterTwoWords() {
-        String result = "a: ab\n"+
+        String result = "a: ab\n" +
                 "b: ab";
         App app = new App();
         assertEquals(result, app.indexing("ab ab"));
     }
+
+    @Test
+    public void isDemoTest() {
+        String input = "ala ma kota, kot koduje w Javie Kota";
+        String result =
+                "a: ala, javie, kota, ma\n" +
+                        "d: koduje\n" +
+                        "e: javie, koduje\n" +
+                        "i: javie\n" +
+                        "j: javie, koduje\n" +
+                        "k: koduje, kot, kota\n" +
+                        "l: ala\n" +
+                        "m: ma\n" +
+                        "o: koduje, kot, kota\n" +
+                        "t: kot, kota\n" +
+                        "u: koduje\n" +
+                        "v: javie\n" +
+                        "w: w";
+
+        App app = new App();
+        assertEquals(result, app.indexing(input));
+    }
+
+
 }
