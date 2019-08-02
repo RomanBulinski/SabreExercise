c5b2b334ab078a61eae64a2f905415162c1e8121
Roman Bulinski <aboandrus@gmail.com>
2019-08-02 07:36:32 +0200

    Refactor code

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,18 +5,13 @@ public class App {
 
     public String indexing(String input) {
 
-        if (input == null) {
-            return "Wrong input!!!";
-        }
-        if (input.length() == 0) {
-            return "Empty input!!!";
-        }
+        if (input == null) return "Wrong input!!!";
+        if (input.length() == 0) return "Empty input!!!";
 
         String cleanedInput = input.replaceAll("[^a-zA-Z0-9 ]", "").toLowerCase();
         String[] words = cleanedInput.split(" ");
 
         TreeMap<String, List<String>> indexes = buildIndexes(cleanedInput);
-
         infillIndexes(words, indexes);
 
         String result = indexes.keySet().stream()
